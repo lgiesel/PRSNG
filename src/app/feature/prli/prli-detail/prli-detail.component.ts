d41dc1908bf4775d7d7d6e39d4565bce5050de93
lgiesel <lgiesel543@yahoo.com>
2018-01-09 11:45:21 -0500

    Added prli dtl btn on prli, added prli detail page

--- src/app/feature/prli/prli-detail/prli-detail.component.ts
+++ src/app/feature/prli/prli-detail/prli-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { PrliService } from '@svc/prli.service';
+import { PurchaseRequestLineItem } from '@model/purchaserequestlineitem';
 
 @Component({
   selector: 'app-prli-detail',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrliDetailComponent implements OnInit {
 
-  constructor() { }
+  title: string = 'PurchaseRequest Lineitem Detail';
+  Id: string;
+  resp: any;
+
+  prli: PurchaseRequestLineItem;	
+
+  remove(){
+    console.log("this.pr.Id:", this.prli.Id);
+  	this.PRLISvc.remove(this.prli.Id)
+  		.subscribe(resp => {
+  			this.resp = resp;
+  			console.log("PurchaseRequestLI-Detail-Remove:", this.resp);
+  			this.router.navigate(['/prli/list']);
+  		});
+  }
+
+  constructor(private PRLISvc: PrliService,
+  	          private router: Router,
+  	          private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+  	this.route.params.subscribe(parms => this.Id = parms ['id']); 
+    this.PRLISvc.get(this.Id)
+      .subscribe(prli => {
+        this.prli = prli.length > 0 ? prli[0] : null;
+        console.log(this.prli);
+      });
+
   }
 
 }
