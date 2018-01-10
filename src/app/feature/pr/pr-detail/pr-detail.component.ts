6c1482cec532820190959e638f6505f9bbe9c35d
lgiesel <lgiesel543@yahoo.com>
2018-01-10 11:59:34 -0500

    Added status desc to pr detail

--- src/app/feature/pr/pr-detail/pr-detail.component.ts
+++ src/app/feature/pr/pr-detail/pr-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { PrService } from '@svc/pr.service';
 import { PurchaseRequest } from '@model/purchaserequest';
+import { StatusService } from '@svc/status.service';
+import { Status } from '@model/status';
 
 @Component({
   selector: 'app-pr-detail',
@@ -13,8 +15,8 @@ export class PrDetailComponent implements OnInit {
   title: string = 'PurchaseRequest Detail';
   Id: string;
   resp: any;
-
   pr: PurchaseRequest;	
+  status: Status[];
 
   remove(){
     console.log("this.pr.Id:", this.pr.Id);
@@ -26,7 +28,16 @@ export class PrDetailComponent implements OnInit {
   		});
   }
 
-  constructor(private PrSvc: PrService,
+  addStatusDesc(pr: PurchaseRequest) {
+    this.StatusSvc.get(pr.StatusID)
+     .subscribe(Status => {
+       pr.StatusDesc = Status[0].Description;
+       console.log("addStatusDesc: " + pr);
+     });
+   }     
+
+  constructor(private PrSvc: PrService, 
+              private StatusSvc: StatusService,
   	          private router: Router,
   	          private route: ActivatedRoute) { }
 
@@ -35,8 +46,9 @@ export class PrDetailComponent implements OnInit {
     this.PrSvc.get(this.Id)
       .subscribe(pr => {
         this.pr = pr.length > 0 ? pr[0] : null;
+        this.addStatusDesc(this.pr);        
         console.log(this.pr);
-      });
+    });
   }
 
 }
